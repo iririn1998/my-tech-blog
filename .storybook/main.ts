4a13e4d91500baaf72d3db1256fcae3a04da0a84
iririn1998 <iririn199810710@gmail.com>
2026-07-19 21:38:34 +0900

    chore: prepare Astro blog tooling

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,9 @@
-export default {
+import { defineMain } from '@storybook-astro/framework/node';
+
+export default defineMain({
 	stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
 	framework: {
 		name: '@storybook-astro/framework',
 		options: {},
 	},
-};
+});
